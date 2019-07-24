73da4ef548be603fd418b0a643de5c5c35d514a7
melgendi <marwan.el-gendi@pwc.com>
2019-07-24 16:52:38 +0100

    fixed the close function to actually work...in theory

--- poisonberry/beacon.py
+++ poisonberry/beacon.py
@@ -8,7 +8,7 @@ import sys
 from bs4 import BeautifulSoup, Comment
 from utils import *
 
-
+#TODO autostart, 4G incorporation, network checker.
 
 
 class poisonberry:
@@ -25,64 +25,76 @@ class poisonberry:
 
     def art(self):
         print("""\                            
-                                            
-                            ____        _                  __                        
-                           / __ \____  (_)________  ____  / /_  ___  ____________  __
-                          / /_/ / __ \/ / ___/ __ \/ __ \/ __ \/ _ \/ ___/ ___/ / / /
-                         / ____/ /_/ / (__  ) /_/ / / / / /_/ /  __/ /  / /  / /_/ / 
-                        /_/    \____/_/____/\____/_/ /_/_.___/\___/_/  /_/   \__, /  
-                                                                            /____/                     
-                                            
-                                               ._ o o
-                                               \_`-)|_
-                                            ,""       \ 
-                                          ,"  ## |   ಠ ಠ. 
-                                        ," ##   ,-\__    `.
-                                      ,"       /     `--._;) Why tho
-                                    ,"     ## /
-                                  ,"   ##    /
-                                  
 
-                            """)
+
+                  ____   ____ _______ ______   __
+                 |  _ \ / __ \__   __/ __ \ \ / /
+                 | |_) | |  | | | | | |  | \ V / 
+                 |  _ <| |  | | | | | |  | |> <  
+                 | |_) | |__| | | | | |__| / . \ 
+                 |____/ \____/  |_|  \____/_/ \_\\
+                                                             _
+                                                            (_)
+                                      |    .
+                                  .   |L  /|   .          _
+                              _ . |\ _| \--+._/| .       (_)
+                             / ||\| Y J  )   / |/| ./
+                            J  |)'( |        ` F`.'/        _
+                          -<|  F         __     .-<        (_)
+                            | /       .-'. `.  /-. L___       
+                            J \      <    \  | | O\|.-'  _   
+                          _J \  .-    \/ O | | \  |F    (_) 
+                         '-F  -<_.     \   .-'  `-' L__    
+                        __J  _   _.     >-'  )._.   |-'  
+                        `-|.'   /_.           \_|   F    
+                          /.-   .                _.<     
+                         /'    /.'             .'  `\    
+                          /L  /'   |/      _.-'-\\
+                         /'J       ___.---'\|
+                           |\  .--' V  | `. `
+                           |/`. `-.     `._)
+                              / .-.\\
+                              \ (  `\\
+                               `.\\
+                                                
+                                                LOADING......    """)
         time.sleep(5)
 
     def start(self):
-       # self.art()
+       # self.art() #gotta have that art
         while True: # run forever
+
                 self.GET()              # http beacon
                 log(INFO,"pausing before next beacon")
-                time.sleep(5)           # sleep between beacons
+                if self.running ==0 :
+                     time.sleep(5)           # sleep between beacons when no tunnel
+                else:
+                    time.sleep(30)           # longer sleep while we have tunnel
 
-    def close_tunnel(self):
-        host="0.0.0.0"
-        port = self.port
 
-        s = None
-        for res in socket.getaddrinfo(host, port, socket.AF_UNSPEC, socket.SOCK_STREAM):
-            af, socktype, proto, canonname, sa = res
 
-            s.close()
 
-        '''
-        #old socket stuff, keep for reference for now but dump if useless
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        result = False
-
-        try:
-
-        #todo add something that frees the port when the server exits the tunnel --> would it need to be a beacon?
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        result = False
-        try:
-            sock.bind(("0.0.0.0", self.port))
-            print("port is free")
-            self.beacon = True
-            result = True
-        except:
-            print("Port is in use")
-        sock.close()
-        return result
-        '''
+    '''
+            #old socket stuff, keep for reference for now but dump if useless
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            result = False
+    
+            try:
+    
+            #todo add something that frees the port when the server exits the tunnel --> would it need to be a beacon?
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            result = False
+            try:
+                sock.bind(("0.0.0.0", self.port))
+                print("port is free")
+                self.beacon = True
+                result = True
+            except:
+                print("Port is in use")
+            sock.close()
+            return result
+            
+    '''
 
     def GET(self):
          self.handle_request()
@@ -153,13 +165,31 @@ class poisonberry:
                 log(DEBUG,"running")
                 print("running")
             '''
+    def close_tunnel(self):
+        #TODO test this command on a box
+        cmd = 'sudo netstat -lpn | grep":'+self.port+'\b " | awk \'{sub(/\/.*/, "", $NF); print $NF}\' | xargs -i kill -kill {}'
+        log(DEBUG,"command is: "+cmd)
+        os.system(cmd)
+
+    '''
+        #TODO this almost definitely doesnt work, maybe we use the -S flag for createing a session file OR  maybe using a different GO command?
+        host="0.0.0.0"
+        port = self.port
+        s = None
+        for res in socket.getaddrinfo(host, port, socket.AF_UNSPEC, socket.SOCK_STREAM):
+            af, socktype, proto, canonname, sa = res
+
+            s.close()
+    '''
 
     def ssh_tunnel(self):
+        #todo work out why this part exists
         log(INFO, "write this part")
 
 
-c=poisonberry()
 
+c=poisonberry()
+#c.art()
 c.start()
 #c.GET()
 
